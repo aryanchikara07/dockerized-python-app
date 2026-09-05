96f8d31627bac8045bbc77d089a41f6e7d672d25
aaryanjaat10-wq <aryanchikara10@gmail.com>
2026-09-05 15:26:30 +0530

    Test Elastic IP deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Aryan's Cloud Application - CI/CD"
+    return "Welcome to Aryan's Cloud Application - HTTPS Ready"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
